41261a60725a908a16d347a395a463c9fd349256
Yuri Batan <batanyuri@gmail.com>
2023-03-07 11:45:38 -0600

    Added DOM to replace Alerts for User Interface

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let computerWins = 0;
+let playerWins = 0;
 
 const getComputerChoice = () => {
     let x = Math.floor(Math.random() * 3 + 1)
@@ -12,14 +14,31 @@ const getComputerChoice = () => {
     }
 }
 const runGame = (playerSelection, computerSelection) => {
+  let displayResult = document.getElementById('Result');
+  let displayComputer = document.getElementById('Computer');
+  let playerComputer = document.getElementById('You');
+
+  
   if (playerSelection.toLowerCase() === computerSelection) {
-    alert("Draw")
-    return "Draw"
+    displayResult.innerHTML = "Result: Draw";
   } else if ((playerSelection.toLowerCase () == "rock" && computerSelection == "scissors") || (playerSelection.toLowerCase () == "scissors" && computerSelection == "paper") || (playerSelection.toLowerCase () == "paper" && computerSelection == "rock")) {
-    alert("Win")
-    return "Win! "+playerSelection+" beats "+computerSelection+ "!"
+    displayResult.innerHTML = "Result: You Win";
+    playerWins++;
   } else if ((playerSelection.toLowerCase () == "rock" && computerSelection == "paper") || (playerSelection.toLowerCase () == "scissors" && computerSelection == "rock") || (playerSelection.toLowerCase () == "paper" && computerSelection == "scissors")) {
-    alert("Loss")
-    return "Loss... "+computerSelection+" beats "+playerSelection+ "!"
+    displayResult.innerHTML = "Result: You Lose :(";
+    computerWins++;
   }
+
+  if(computerWins == 5){
+    alert("The Computer Wins. Resetting Game")
+    computerWins = 0;
+    playerWins = 0;
+  } else if (playerWins == 5) {
+    alert("You Win. Resetting Game")
+    computerWins = 0;
+    playerWins = 0;
+  }
+
+  displayComputer.innerHTML = computerWins;
+  playerComputer.innerHTML = playerWins;
 }
